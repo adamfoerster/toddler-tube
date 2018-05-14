5fc01cb72d8fa9545f82618bbe9ed262f6e27ed9
adamfoerster <adam.foerster@gmail.com>
2018-05-13 21:37:01 -0300

    prototype

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,40 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { PopoverController } from 'ionic-angular';
+import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/operators';
+
+import { PasswordPage } from '../password/password';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  active = false;
+  popoverShow = false;
+  popover: any;
+  timecode: number = 0;
+  obsTimecode: any;
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, private pop: PopoverController) {
+    this.obsTimecode = Observable.of
+    this.timecode
   }
 
+  activate() {
+    if (this.active){
+      console.log('calling popover');
+      this.popover = this.pop.create(PasswordPage);
+      this.popover.present();
+      this.popover.onDidDismiss(_=>{
+        this.popoverShow = false;
+      });
+      this.popoverShow = true;
+      this.active = !this.active;
+    } else {
+      console.log('activating');
+      this.active = !this.active;
+    }
+  }
 }
---
+++ src/pages/password/password.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-password',
+  templateUrl: 'password.html'
+})
+export class PasswordPage {
+  constructor() {}
+}
